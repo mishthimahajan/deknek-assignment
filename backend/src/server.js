04fd2664b1dee2e3f8240b6dc6cadeaaab0e0f85
mishthimahajan <mishthimahajan0@gmail.com>
2026-04-24 01:59:10 +0530

    Test minimal Render server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,42 +1,14 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js";
-import taskRoutes from "./routes/taskRoutes.js";
-
-dotenv.config();
 
 const app = express();
 
-connectDB();
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://deknek-assignment-xi.vercel.app"
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
-app.use("/api/auth", authRoutes);
-app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 
