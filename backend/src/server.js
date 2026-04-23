caa2f091f68d5c6f42e242c16d3af318b85fc45e
mishthimahajan <mishthimahajan0@gmail.com>
2026-04-24 02:03:36 +0530

    change

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
